86c643ac9b4874e6185f2b30fd4838a321d8081f
Akshay Neje <akshayneje2823@gmail.com>
2022-05-31 12:44:12 +0530

    Card Completed

--- src/Zomato Card/Cards.jsx
+++ src/Zomato Card/Cards.jsx
@@ -1,22 +1,40 @@
 import React from 'react';
 import Card from 'react-bootstrap/Card'
 
-function Cards() {
+function Cards({ data }) {
     return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Img variant="top" src="holder.js/100px180" />
-            <div className="card_body">
-                <div className="upper_data d-flex justify-content-between align-items-center">
-                    <h4 className='mt-2'>Masala Theoryy</h4>
-                    <span>3.8&nbsp;★</span>
-                </div>
 
-                <div className="lower_data d-flex justify-content-between">
-                    <h5>North Indian,Birayani,Mughalai</h5>
-                    <span>350 for one</span>
-                </div>
-            </div>
-        </Card>
+        <>
+            {
+                data.map((element, k) => {
+                    return (
+                        <Card style={{ width: '22rem',border:"none" }} className="hover mb-4">
+                            <Card.Img variant="top" className='cd' src={element.imgdata} />
+                            <div className="card_body">
+                                <div className="upper_data d-flex justify-content-between align-items-center">
+                                    <h4 className='mt-2'>{element.rname}</h4>
+                                    <span>{element.rating}&nbsp;★</span>
+                                </div>
+
+                                <div className="lower_data d-flex justify-content-between">
+                                    <h5>{element.address}</h5>
+                                    <span>{element.price}</span>
+                                </div>
+                            </div>
+                            <div className="extra">
+                            </div>
+
+                            <div className="last_data d-flex justify-content-between align-items-center">
+                                <img src={element.arrimg} className="limg" alt="img" />
+                                <p>{element.somedata}</p>
+                                <img src={element.delimg} className="laimg" alt="img" />
+                            </div>
+                        </Card>
+                    )
+                })
+            }
+
+        </>
     )
 }
 
